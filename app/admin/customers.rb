ad477a82730897783c1ae1dd9bd9247cc85b54bd
hhua53 <hhxqweasd@gmail.com>
2020-11-09 23:05:17 -0600

    add upload image function

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -2,6 +2,15 @@ ActiveAdmin.register Customer do
 
 
   permit_params :name, :number, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
   #
   # or
   #
